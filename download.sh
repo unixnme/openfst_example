a81b82bebd21dee06cda9a08d989cb664320c62c
Unix&Me <unixnme@gmail.com>
2021-09-12 12:14:20 -0700

    add download script

---
+++ download.sh
@@ -0,0 +1,8 @@
+set -xe
+
+for file in wotw.txt wotw.lm.gz wotw.syms ascii.syms lexicon.txt.gz lexicon_opt.txt.gz full_downcase.txt; do
+    if [ ! -f "$file" ]; then
+        wget http://www.openfst.org/twiki/pub/FST/FstExamples/$file
+    fi
+done
+
