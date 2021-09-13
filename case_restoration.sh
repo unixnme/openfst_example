bea9b01b760bd21876dd888dffc10bba46bf0321
Unix&Me <unixnme@gmail.com>
2021-09-12 20:26:18 -0700

    add case_restoration which works on linux only for now

---
+++ case_restoration.sh
@@ -0,0 +1,16 @@
+set -xe
+
+gunzip -c wotw.lm.gz > wotw.lm
+fstcompile --isymbols=wotw.syms --osymbols=wotw.syms wotw.lm ngram.fst
+
+fstcompile --isymbols=ascii.syms --osymbols=ascii.syms full_downcase.txt full_downcase.fst
+
+# option a (slow)
+# fstcompose lexicon_opt.fst ngram.fst | fstarcsort --sort_type=ilabel > wotw.fst
+
+# option b (fast)
+fstconvert --fst_type=olabel_lookahead --save_relabel_opairs=relabel.pairs lexicon_opt.fst > lexicon_lookahead.fst
+fstrelabel --relabel_ipairs=relabel.pairs ngram.fst | fstarcsort --sort_type=ilabel > ngram_relabel.fst
+fstcompose lexicon_lookahead.fst ngram_relabel.fst > wotw.fst
+
+fstinvert full_downcase.fst | fstcompose - wotw.fst >case_restore.fst
--- download.sh
+++ download.sh
@@ -1,6 +1,14 @@
 set -xe
 
-for file in wotw.txt wotw.lm.gz wotw.syms ascii.syms lexicon.txt.gz lexicon_opt.txt.gz full_downcase.txt full_punct.txt; do
+for file in wotw.txt \
+ wotw.lm.gz \
+ wotw.syms \
+ ascii.syms \
+ lexicon.txt.gz \
+ lexicon_opt.txt.gz \
+ full_downcase.txt \
+ full_punct.txt \
+ full_downcase.txt; do
     if [ ! -f "$file" ]; then
         wget http://www.openfst.org/twiki/pub/FST/FstExamples/$file
     fi
